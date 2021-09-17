c1181a1b3bf9b32a2138db019c9e27566d1b4cc4
Vanya Dimitrova <vani4ka66@abv.bg>
2021-09-17 15:39:54 +0300

    Commit 1

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import "../scss/app.scss";
+const R = require("ramda");
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
@@ -9,4 +10,14 @@ window.addEventListener("DOMContentLoaded", () => {
     { job: "programmer", class: "is-danger" },
   ];
   const articles = document.querySelectorAll("article");
+
+  let getClass = R.pluck('class')
+  getClass(arrayToPluck)
+  console.log(getClass(arrayToPluck))
+
+  for(var i in articles){
+    // console.log(articles[i])
+    articles[i].classList.add(getClass(arrayToPluck)[i])
+  }
+  
 });
